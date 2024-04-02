a693db79d0a8ef6e48344b5e518a261aa19e6d53
my pc <ashishkumarjha@ssccglpinnacle.com>
2024-04-02 17:05:49 +0530

    first

---
+++ app/src/main/java/eu/tutorials/a7minutesworkout/WorkOutApp.kt
@@ -0,0 +1,10 @@
+package eu.tutorials.a7minutesworkout
+
+import android.app.Application
+
+class WorkOutApp: Application() {
+
+    val db by lazy {
+        HistoryDatabase.getInstance(this)
+    }
+}
